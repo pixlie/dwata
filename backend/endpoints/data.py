08f5ee46644e5d079340f4a9d6a129d363396c20
Sumit Datta <sumitdatta@gmail.com>
2020-05-09 17:43:17 +0700

    (task, #64) API: Pagination for SQL data sources

--- backend/endpoints/data.py
+++ backend/endpoints/data.py
@@ -1,6 +1,6 @@
 from starlette.responses import Response
 import sqlalchemy
-from sqlalchemy import create_engine, MetaData, select, column, table, text
+from sqlalchemy import create_engine, MetaData, select, column, table, text, func
 from json.decoder import JSONDecodeError
 
 from utils.response import RapidJSONResponse, web_error
@@ -13,6 +13,53 @@ example_spec = {
     "columns": ["client.id", "client.joined_at"],
     "limit": 300
 }
+
+
+def apply_ordering(query_specification, sel_obj, current_table):
+    order_by = query_specification["order_by"]
+    valid_order_by = [
+        (col, ord_type if ord_type in ("asc", "desc") else "asc")
+        for col, ord_type in order_by.items() if col in current_table.columns.keys()
+    ]
+    sel_obj.order_by(*[getattr(sqlalchemy, x[1])(getattr(current_table.c, x[0])) for x in valid_order_by])
+    return sel_obj
+
+
+def apply_filters(query_specification, sel_obj, current_table):
+    filter_by = query_specification["filter_by"]
+    for col, filter_spec in filter_by.items():
+        if col in current_table.columns:
+            column_def = current_table.columns[col]
+            if str(column_def.type) == "INTEGER":
+                # We can do an equals query or a range query
+                if filter_spec.get("equal", None):
+                    sel_obj = sel_obj.where(column(col) == filter_spec["equal"])
+                elif filter_spec.get("from", None) and filter_spec.get("to", None):
+                    sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
+                    sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
+                elif filter_spec.get("from", None) and not filter_spec.get("to", None):
+                    sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
+                elif filter_spec.get("to", None) and not filter_spec.get("from", None):
+                    sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
+            elif "VARCHAR" in str(column_def.type):
+                if filter_spec.get("like", None):
+                    sel_obj = sel_obj.where(column(col).ilike("%{}%".format(filter_spec["like"])))
+            elif "TIMESTAMP" in str(column_def.type):
+                if filter_spec.get("equal", None):
+                    sel_obj = sel_obj.where(column(col) == filter_spec["equal"])
+                elif filter_spec.get("from", None) and filter_spec.get("to", None):
+                    sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
+                    sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
+                elif filter_spec.get("from", None) and not filter_spec.get("to", None):
+                    sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
+                elif filter_spec.get("to", None) and not filter_spec.get("from", None):
+                    sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
+            elif str(column_def.type) == "BOOLEAN" and filter_spec.get("value", None) is not None:
+                if filter_spec["value"] is True:
+                    sel_obj = sel_obj.where(column(col))
+                else:
+                    sel_obj = sel_obj.where(~column(col))
+    return sel_obj
 
 
 async def data_post(request):
@@ -44,76 +91,34 @@ async def data_post(request):
         conn.close()
         return Response("", status_code=404)
 
-    tt = meta.tables[table_name]
-    sel = {
-        "limit": query_specification.get("limit", 100)
-    }
+    current_table = meta.tables[table_name]
     if len(query_specification.get("columns", [])) > 0:
-        sel["columns"] = [column(col) for col in query_specification["columns"]]
+        columns = [column(col) for col in query_specification["columns"]]
     else:
-        sel["columns"] = [text("*")]
+        columns = [text("*")]
 
-    if query_specification.get("order_by", None):
-        order_by = query_specification["order_by"]
-        valid_order_by = [
-            (col, ord_type if ord_type in ("asc", "desc") else "asc")
-            for col, ord_type in order_by.items() if col in tt.columns.keys()
-        ]
-        sel["order_by"] = (getattr(sqlalchemy, x[1])(getattr(tt.c, x[0])) for x in valid_order_by)
-
-    sel_obj = select(**sel)
+    sel_obj = select(columns)
     sel_obj = sel_obj.select_from(table(table_name))
-
+    if query_specification.get("order_by", None):
+        sel_obj = apply_ordering(query_specification, sel_obj, current_table)
     if query_specification.get("filter_by", None):
-        filter_by = query_specification["filter_by"]
-        for col, filter_spec in filter_by.items():
-            if col in tt.columns:
-                column_def = tt.columns[col]
-                if str(column_def.type) == "INTEGER":
-                    # We can do an equals query or a range query
-                    if filter_spec.get("equal", None):
-                        sel_obj = sel_obj.where(column(col) == filter_spec["equal"])
-                    elif filter_spec.get("from", None) and filter_spec.get("to", None):
-                        sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
-                        sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
-                    elif filter_spec.get("from", None) and not filter_spec.get("to", None):
-                        sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
-                    elif filter_spec.get("to", None) and not filter_spec.get("from", None):
-                        sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
-                elif "VARCHAR" in str(column_def.type):
-                    if filter_spec.get("like", None):
-                        sel_obj = sel_obj.where(column(col).ilike("%{}%".format(filter_spec["like"])))
-                elif "TIMESTAMP" in str(column_def.type):
-                    if filter_spec.get("equal", None):
-                        sel_obj = sel_obj.where(column(col) == filter_spec["equal"])
-                    elif filter_spec.get("from", None) and filter_spec.get("to", None):
-                        sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
-                        sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
-                    elif filter_spec.get("from", None) and not filter_spec.get("to", None):
-                        sel_obj = sel_obj.where(column(col) >= filter_spec["from"])
-                    elif filter_spec.get("to", None) and not filter_spec.get("from", None):
-                        sel_obj = sel_obj.where(column(col) <= filter_spec["to"])
-                elif str(column_def.type) == "BOOLEAN" and filter_spec.get("value", None) is not None:
-                    if filter_spec["value"] is True:
-                        sel_obj = sel_obj.where(column(col))
-                    else:
-                        sel_obj = sel_obj.where(~column(col))
+        sel_obj = apply_filters(query_specification, sel_obj, current_table)
+    sel_obj = sel_obj.limit(query_specification.get("limit", 100))
+    sel_obj = sel_obj.offset(query_specification.get("offset", 0))
     exc = conn.execute(sel_obj)
 
     # We use a separate query to count the total number of rows in the given query
-    # first_column = tt.columns[tt.columns.keys()[0]]
-    # count_sel = {
-    #     **sel,
-    #     "columns": [func.count(first_column)]
-    # }
-    # del count_sel["limit"]
-    # count = conn.execute(select(**count_sel)).scalar()
+    count_sel_obj = select([func.count(text("*"))]).select_from(table(table_name))
+    if query_specification.get("filter_by", None):
+        count_sel_obj = apply_filters(query_specification, count_sel_obj, current_table)
+    count = conn.execute(count_sel_obj).scalar()
     conn.close()
+
     return RapidJSONResponse(
         dict(
             columns=exc.keys(),
             rows=exc.cursor.fetchall(),
-            # count=count,
+            count=count,
             query_sql=str(sel_obj),
         )
     )
