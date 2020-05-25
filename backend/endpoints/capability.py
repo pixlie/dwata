1c58fc9d32b67baacb00f397548f0b019a4e7cd3
Sumit Datta <sumitdatta@gmail.com>
2020-05-25 18:55:27 +0700

    [WIP] (task) API: Minor changes to get Notes working [ch142]

--- backend/endpoints/capability.py
+++ backend/endpoints/capability.py
@@ -1,14 +1,18 @@
 from utils.response import RapidJSONResponse
 
 
-async def capability_get():
+async def capability_get(_):
     """Get the list of dwata provided capabilities that have been configured"""
     # Todo: this is a hack, please update in [ch162]
     return RapidJSONResponse({
         "columns": [
-            "label", "is_enabled", "status"
+            "label", "is_enabled", "properties"
         ],
         "rows": [
-            ["notes", True, "in_use"]
+            ["notes", True, {
+                "in_use": True,
+                "source_id": 0,
+                "table_name": "admin_meta_notes",
+            }]
         ]
     })
