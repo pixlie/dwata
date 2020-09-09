883de74329be59138386819f0e30335b4a346d64
Sumit Datta <sumitdatta@gmail.com>
2020-09-09 16:51:52 +0530

    (task) Update to the Product website

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -1,179 +1,134 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect } from "react";
 
 import { Section, Hx } from "components/LayoutHelpers";
 import SignupEarlyAccess from "components/SignupEarlyAccess";
-import { Founder, SoftwareEngineer, ProductManager, MarketerSalesExec } from "components/AudiencePitch";
-import KanbanViewScreenshot from "assets/dwata_screenshot_Kanban_view.png";
-import DetailViewScreenshot from "assets/dwata_screenshot_Detail_view.png";
-import TableFiltersScreenshot from "assets/dwata_screenshot_Table_Filters.png";
-import NotesScreenshot from "assets/dwata_screenshot_Notes.png";
-
-
-const AudiencePitch = () => {
-  const [ state, setState ] = useState({
-    selectedAudience: "Founder",
-  });
-  const { selectedAudience } = state;
-
-  const Audience = ({ title }) => {    
-    const setAudience = event => {
-      if (selectedAudience === title) {
-        setState({
-          selectedAudience: null,
-        })
-      } else {
-        setState({
-          selectedAudience: title,
-        });
-      }
-    }
-    const classes = selectedAudience === title ?
-      "font-bold bg-gray-700 text-white p-2 px-4 mr-2 mb-2 md:mb-0 rounded-full shadow inline-block cursor-pointer" :
-      "font-bold bg-green-300 p-2 px-4 mr-2 mb-2 md:mb-0 rounded-full shadow inline-block cursor-pointer"
- 
-    return (
-      <div
-        className={classes}
-        onClick={setAudience}>{title}</div>
-    );
-  }
-
-  return (
-    <Fragment>
-      <div className="max-w-screen-md flex bg-white rounded-lg p-4 mx-2 md:mx-auto">
-        <img className="h-16 w-16 md:h-24 md:w-24 rounded-full mr-6 mb-3"
-          alt="Avatar of Sumit Datta, founder of dwata"
-          src="https://ph-avatars.imgix.net/1966591/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop" />
-
-        <p className="text-lg">
-          Hello and welcome!<br />I'm Sumit and I've worked with 10+ startups over 14 years as an Engineer.
-          I help Businesses scale their Operations without writing Code.
-          <br />&nbsp;<br />What's your professional role?
-        </p>
-      </div>
-
-      <div className="container mx-2 md:mx-0 md:flex md:justify-center my-3">
-        <Audience title="Founder" />
-        <Audience title="Software Engineer" />
-        <Audience title="Product Manager" />
-        <Audience title="Marketer/Sales Exec" />
-      </div>
-
-      <div className="max-w-screen-lg mx-2 md:mx-auto">
-        { selectedAudience === "Founder" ? <Founder /> : null }
-        { selectedAudience === "Software Engineer" ? <SoftwareEngineer /> : null }
-        { selectedAudience === "Product Manager" ? <ProductManager /> : null }
-        { selectedAudience === "Marketer/Sales Exec" ? <MarketerSalesExec /> : null }
-      </div>
-    </Fragment>
-  )
-}
+import RelatedAndFiltersTour1 from "assets/Product_Tour/Related_and_Filters_Tour_1.png";
+import RelatedAndFiltersTour2 from "assets/Product_Tour/Related_and_Filters_Tour_2.png";
+import RelatedAndFiltersTour3 from "assets/Product_Tour/Related_and_Filters_Tour_3.png";
+import RelatedAndFiltersTour5 from "assets/Product_Tour/Related_and_Filters_Tour_5.png";
 
 export default () => {
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
   }, []);
 
   return (
     <Fragment>
       <Section extraClasses="bg-white">
         <div className="max-w-screen-lg md:py-24 mx-2 md:mx-auto">
-          <Hx x="1">Scale Your Business Operations</Hx>
-          <Hx x="2" titleClass="subtitle">Without Investing in Software</Hx>
+          <Hx x="1">Manage Databases without knowing SQL</Hx>
           <p className="text-3xl">
-            <strong>dwata</strong> helps your team manage &amp; understand the Business without fighting with SQL databases or third-party APIs.
+            <strong>dwata</strong> enables you to understand and manage Business
+            data without knowing SQL or code.
           </p>
         </div>
-
-        {/* <div className="bg-yellow-400 py-1 px-2 inline-block rounded">Signup to get early access!</div> */}
-
-        {/* <div className="max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="max-w-xl my-12 p-6 bg-gray-100 shadow-md">
-            <Hx x="4">Scaling your business?</Hx>
-            <div className="text-lg">
-              <p>
-                <strong>dwata</strong> makes it easy to onboard your entire team and empower them to
-                understand and modify your Business data. There is <strong>no need to learn SQL</strong>;
-                visually query all the data, share queries, create dashboards and explore.
-              </p>
-            </div>
-          </div>
-
-          <div className="max-w-xl my-12 p-6 bg-gray-100 shadow-md">
-            <Hx x="4">Are you a developer?</Hx>
-            <div className="text-lg">
-              <p>
-                <strong>dwata</strong> gives you <strong>content management, CRUD, visual queries</strong>,
-                and much more. Works with your existing backend, database and APIs directly.
-                It is on-premises for complete privacy. Give your team members an excellent admin!
-              </p>
-            </div>
-          </div>
-        </div> */}
       </Section>
 
-      <Section>
-        <AudiencePitch />
-      </Section>
-
-      {/* <Section>
-        <Hx x="2" titleClass="subtitle">Do you want to understand?</Hx>
-        <ul>
-          <li className="text-2xl">Where users spend time on our product?</li>
-          <li className="text-2xl">Do they interact with our new features?</li>
-          <li className="text-2xl">Or if your efforts are not having impact?</li>
-        </ul>
-      </Section> */}
+      <div className="bg-indigo-100 py-24">
+        <div className="mx-auto" style={{ maxWidth: "1100px" }}>
+          <Hx x="3">Familiar UX for Databases, no SQL needed</Hx>
+          <p className="my-4 p-4 bg-red-200 rounded-lg text-lg font-bold">
+            <strong>dwata</strong> finds out all tables, their columns and
+            relations, and gives you a Spreadsheet like UX
+          </p>
+        </div>
+        <div
+          className="mx-auto rounded-lg border-4"
+          style={{ maxHeight: "600px", maxWidth: "1100px", overflow: "hidden" }}
+        >
+          <img
+            className="w-full"
+            src={RelatedAndFiltersTour1}
+            alt="Screenshot of a grid for a table"
+          />
+        </div>
+      </div>
 
       <SignupEarlyAccess />
 
-      <Section>
-        <div className="max-w-screen-lg mx-2 md:mx-auto text-center">
-          <Hx x="2">Understand User Behaviour with Funnels</Hx>
+      <div className="bg-purple-100 py-24">
+        <div className="mx-auto" style={{ maxWidth: "1100px" }}>
+          <Hx x="3">Merge multiple Tables, just by checking the box</Hx>
+          <p className="my-4 p-4 bg-orange-200 rounded-lg text-lg font-bold">
+            The SQL is generated for you, including JOIN, sub-query, Grouping,
+            Aggregate
+          </p>
         </div>
-
-        <img
-          className="w-full mx-auto rounded-lg shadow-lg"
-          style={{maxWidth: "1100px"}}
-          src={KanbanViewScreenshot}
-          alt="Screenshot of funnels to understand user behaviour" />
-      </Section>
-
-      <Section extraClasses="bg-white">
-        <div className="max-w-screen-lg mx-2 md:mx-auto text-center">
-          <Hx x="2">Explore Data and Insights, no SQL Needed</Hx>
+        <div
+          className="mx-auto rounded-lg border-4"
+          style={{ maxHeight: "600px", maxWidth: "1100px", overflow: "hidden" }}
+        >
+          <img
+            className="w-full"
+            src={RelatedAndFiltersTour2}
+            alt="Screenshot of two tables merged in a grid"
+          />
         </div>
+      </div>
 
-        <img
-          className="w-full mx-auto rounded-lg shadow-md"
-          style={{maxWidth: "1000px"}}
-          src={TableFiltersScreenshot}
-          alt="Screenshot of filtering data from Relational database without SQL knowledge" />
-      </Section>
+      {/* <div className="bg-yellow-400 py-1 px-2 inline-block rounded">Signup to get early access!</div> */}
 
-      <Section>
-        <div className="max-w-screen-lg mx-2 md:mx-auto text-center">
-          <Hx x="2">Business Documentation/Notes in dwata</Hx>
+      <div className="mx-auto max-w-xl p-6 border-4 rounded-lg my-32">
+        <Hx x="4">Are you a Founder?</Hx>
+        <div className="text-lg">
+          <p>
+            With <strong>dwata</strong> you can enable everyone in your business
+            to get complex insights with the ease of Spreadsheet. You can also
+            give permissions to modify the data easily.
+          </p>
         </div>
+      </div>
 
-        <img
-          className="w-full mx-auto rounded-lg shadow-lg"
-          style={{maxWidth: "1100px"}}
-          src={NotesScreenshot}
-          alt="Screenshot of Business documentation/notes directly in dwata" />
-      </Section>
-
-      <Section extraClasses="bg-white">
-        <div className="max-w-screen-lg mx-2 md:mx-auto text-center">
-          <Hx x="2">Data Management, Including Relations</Hx>
+      <div className="bg-orange-100 py-24">
+        <div className="mx-auto" style={{ maxWidth: "1100px" }}>
+          <Hx x="3">Columns selection even with Merged data</Hx>
+          <p className="my-4 p-4 bg-red-200 rounded-lg text-lg font-bold">
+            <strong>dwata</strong> generates new SQL as soon as you change
+            Columns/Filters, including JOINs and sub-queries
+          </p>
         </div>
+        <div
+          className="mx-auto rounded-lg border-4"
+          style={{ maxHeight: "600px", maxWidth: "1100px", overflow: "hidden" }}
+        >
+          <img
+            className="w-full"
+            src={RelatedAndFiltersTour3}
+            alt="Screenshot of funnels to understand user behaviour"
+          />
+        </div>
+      </div>
 
-        <img
-          className="w-full mx-auto rounded-lg shadow-md"
-          style={{maxWidth: "1000px"}}
-          src={DetailViewScreenshot}
-          alt="Screenshot of Data management (CRUD) for SQL data, including Relations" />
-      </Section>
+      <div className="mx-auto max-w-xl p-6 border-4 rounded-lg my-32">
+        <Hx x="4">Are you a Developer?</Hx>
+        <div className="text-lg">
+          <p>
+            <strong>dwata</strong> comes with content management, CRUD, and UX
+            for complex queries. You will not have to setup SQL dashboards. It's
+            on-premise for complete privacy.
+          </p>
+        </div>
+      </div>
+
+      <div className="bg-purple-100 py-24">
+        <div className="mx-auto" style={{ maxWidth: "1100px" }}>
+          <Hx x="3">Easily Filter with data-type aware widgets</Hx>
+          <p className="my-4 p-4 bg-orange-200 rounded-lg text-lg font-bold">
+            <strong>dwata</strong> supports Boolean, Integer, Date/Time, etc.
+            Geolocation, JSON types are coming soon
+          </p>
+        </div>
+        <div
+          className="mx-auto rounded-lg border-4"
+          style={{ maxHeight: "600px", maxWidth: "1100px", overflow: "hidden" }}
+        >
+          <img
+            className="w-full"
+            src={RelatedAndFiltersTour5}
+            alt="Screenshot of funnels to understand user behaviour"
+          />
+        </div>
+      </div>
     </Fragment>
   );
-}
+};
--- website/src/components/SignupEarlyAccess.js
+++ website/src/components/SignupEarlyAccess.js
@@ -2,31 +2,59 @@ import React from "react";
 
 import { Section, Hx } from "components/LayoutHelpers";
 
-
 export default () => (
   <Section extraClasses="bg-white">
     <div id="Get_Early_Access" />
 
     <div className="max-w-screen-lg mx-2 md:mx-auto">
-      <Hx x="2">Subscribe &amp; Get early access!</Hx>
-      <div className="text-xl">
-        <p><strong>dwata</strong> is actively developed and we are working with early adopters.</p>
-      </div>
+      <Hx x="2">Subscribe and get early access!</Hx>
+      <p className="text-xl my-2">
+        <strong>dwata</strong> is developed actively and we are looking to pilot
+        with startups/businesses.
+      </p>
       {/* Begin Mailchimp Signup Form */}
-      <div>
+      <div className="rounded-lg bg-gray-200 p-4">
         <form
           action="https://dwata.us8.list-manage.com/subscribe/post?u=637faf509a5896debdab58eda&amp;id=d781c5751b"
-          method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate"
-          target="_blank" noValidate>
+          method="post"
+          id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form"
+          className="validate"
+          target="_blank"
+          noValidate
+        >
           <div>
             <div className="field">
-              <label htmlFor="mce-EMAIL" className="block text-xl font-bold">Subscribe to our newsletter</label>
-              <input className="my-2 text-2xl border rounded py-2 px-4" type="email" name="EMAIL" id="mce-EMAIL" placeholder="someone@domain.com" required />
-              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-                className="text-2xl bg-blue-500 text-white font-bold hover:bg-blue-800 rounded border rounded ml-6 py-2 px-12" />
+              <label htmlFor="mce-EMAIL" className="block text-xl font-bold">
+                Subscribe to our newsletter
+              </label>
+              <input
+                className="my-2 text-2xl border rounded py-2 px-4"
+                type="email"
+                name="EMAIL"
+                id="mce-EMAIL"
+                placeholder="someone@domain.com"
+                required
+              />
+              <input
+                type="submit"
+                value="Subscribe"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+                className="text-2xl bg-blue-500 text-white font-bold hover:bg-blue-800 rounded border rounded ml-6 py-2 px-12"
+              />
             </div>
             {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
-            <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true"><input type="text" name="b_637faf509a5896debdab58eda_d781c5751b" tabIndex="-1" /></div>
+            <div
+              style={{ position: "absolute", left: "-5000px" }}
+              aria-hidden="true"
+            >
+              <input
+                type="text"
+                name="b_637faf509a5896debdab58eda_d781c5751b"
+                tabIndex="-1"
+              />
+            </div>
           </div>
         </form>
       </div>
