4ad2f328c4d787d34d276ee9c4a0e06dfebb2187
Sumit Datta <sumitdatta@gmail.com>
2024-03-17 22:05:38 +0530

    GUI: Markdown render

--- src/widgets/chat/ReplyItem.tsx
+++ src/widgets/chat/ReplyItem.tsx
@@ -1,10 +1,13 @@
 import { Component } from "solid-js";
 import { APIChatReply } from "../../api_types/APIChatReply";
+import { SolidMarkdown } from "solid-markdown";
 
 const ReplyItem: Component<APIChatReply> = (props) => {
   return (
     <div class="bg-zinc-800 p-3 rounded-md">
-      <p class="text-zinc-400 text-sm">{props.message}</p>
+      <div class="text-zinc-400 text-sm">
+        <SolidMarkdown children={props.message} />
+      </div>
     </div>
   );
 };
