254421d3b76f218712976f0fcd83dd26dadfebe8
Sumit Datta <sumitdatta@gmail.com>
2024-05-01 12:42:42 +0530

    UI: Minor TS refactor

--- src/stores/label.tsx
+++ src/stores/label.tsx
@@ -1,6 +1,6 @@
 import { Component, JSX, createContext, useContext } from "solid-js";
 import { createStore } from "solid-js/store";
-import { IChatRoom } from "../utils/types";
+import { IChatRoom, IProviderPropTypes } from "../utils/types";
 import { invoke } from "@tauri-apps/api/core";
 
 interface IStore {
@@ -31,10 +31,6 @@ type TStoreAndFunctions = ReturnType<typeof makeStore>;
 export const labelStore = makeStore();
 
 const LabelContext = createContext<TStoreAndFunctions>(labelStore);
-
-interface IProviderPropTypes {
-  children: JSX.Element;
-}
 
 export const DataSourceProvider: Component<IProviderPropTypes> = (props) => {
   return (
